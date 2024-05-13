d6a279584cb2cea4526f17f99834878ab05c1d6b
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-05-14 02:40:04 +0900

    chore: update Entity

--- joy/src/main/java/com/be/dev/joy/domain/TitleData.java
+++ joy/src/main/java/com/be/dev/joy/domain/TitleData.java
@@ -18,15 +18,15 @@ public class TitleData {
     @Column(name = "title_data_id")
     private Long id; // 도전과제정보 고유 ID
 
-    @Column(length = 50)
+    @Column(length = 50, nullable = false)
     private String header; // 도전과제명
 
-    @Column(length = 50)
+    @Column(length = 50, nullable = false)
     private String body; // 세부 과제 내용
 
-    @Column
+    @Column(nullable = false)
     private Integer goal; // 목표 횟수
 
-    @Column(length = 500, name = "img_url")
+    @Column(length = 500, name = "img_url", nullable = false)
     private String imgUrl; // 이미지
 }
