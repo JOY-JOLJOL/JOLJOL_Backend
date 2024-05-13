3563ea5a564331c607c26f581762cfdcd0b81d23
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-05-14 00:03:11 +0900

    refactor: change enum style

--- joy/src/main/java/com/be/dev/joy/domain/enums/Stamp.java
+++ joy/src/main/java/com/be/dev/joy/domain/enums/Stamp.java
@@ -5,16 +5,18 @@ import lombok.Getter;
 @Getter
 public enum Stamp {
 
-    NOT_GOOD("NOT_GOOD"), // 토닥토닥
-    CHEER_UP("CHEER_UP"), // 아자아자!
-    WELL_DONE("WELL_DONE"), // 수고했어
-    GOOD_BOY("GOOD_BOY"), // 굿보이!
-    BEST("BEST") // 짱!짱!짱!
+    NOT_GOOD("NOT_GOOD", "토닥토닥"),
+    CHEER_UP("CHEER_UP", "아자아자"),
+    WELL_DONE("WELL_DONE", "수고했어"),
+    GOOD_BOY("GOOD_BOY", "굿보이"),
+    BEST("BEST", "짱!짱!짱!") 
     ;
 
     private final String stamp;
+    private final String title;
 
-    Stamp(String stamp) {
+    Stamp(String stamp, String title) {
         this.stamp = stamp;
+        this.title = title;
     }
 }
