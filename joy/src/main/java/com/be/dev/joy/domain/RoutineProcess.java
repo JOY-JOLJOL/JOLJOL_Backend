3992d90cff5e588cd020f169726a2e4e2db4520a
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-05-14 01:38:02 +0900

    chore: update Entity

--- joy/src/main/java/com/be/dev/joy/domain/RoutineProcess.java
+++ joy/src/main/java/com/be/dev/joy/domain/RoutineProcess.java
@@ -5,12 +5,15 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
 
 @Getter
 @Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@DynamicInsert
 public class RoutineProcess {
 
     @Id
@@ -23,5 +26,6 @@ public class RoutineProcess {
     private Routine routine; // 스낵루틴 고유 ID
     
     @Column
+    @ColumnDefault("0")
     private Integer stat; // 이번 주 달성 수치
 }
